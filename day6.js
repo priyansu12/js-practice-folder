4f22f4e42ef8c96273398e41ace5b0cbccdd7934
Priyansu_proves <90149156+priyansu12@users.noreply.github.com>
2024-03-11 19:47:03 +0530

    day6

--- day6.js
+++ day6.js
@@ -11,3 +11,16 @@ const arr = [1,2,43,4,6,7]
 for (let x of arr) {
     console.log(x)
 }
+
+// const letters = new Set(["a", "b", "c"])
+// letters.add("d")
+// console.log(letters)
+const names = new Map(
+    [["firstName", "priyansu"],
+    ["lastName", "sekhar"]]
+    )
+    
+console.log(names.get("firstName"))
+
+console.log(names.set("lastName", "behera"))
+console.log(names.has("lastName"))
